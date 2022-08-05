7d567339e9d7e1e62a2b00e4c25c0ad8c527a021
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2022-08-05 00:10:14 -0300

    Pizza information being pulled and showing to the user.

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ const qSA = (e) => document.querySelectorAll(e);
 pizzaJson.map((item, idex) => {
     let pizzaItem = qS('.models .pizza-item').cloneNode(true)
 
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img
+
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}` 
+
     qS('.pizza-area').append(pizzaItem)
 })
