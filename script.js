bac76eb10b6ed508795f3f21790d7daf3607f361
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2022-08-20 16:39:59 -0300

    Information being sent to cart.

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+let cart = [];
+
+let modalKey = "";
+
 const qS = (e) => document.querySelector(e);
 const qSA = (e) => document.querySelectorAll(e);
-let modalQt = 1
+let modalQt = 1;
 
 pizzaJson.map((item, index) => {
   let pizzaItem = qS(".models .pizza-item").cloneNode(true);
 
-  pizzaItem.setAttribute('data-key', index);
+  pizzaItem.setAttribute("data-key", index);
 
   pizzaItem.querySelector(".pizza-item--img img").src = item.img;
 
@@ -18,23 +22,29 @@ pizzaJson.map((item, index) => {
   pizzaItem.querySelector("a").addEventListener("click", (event) => {
     event.preventDefault();
 
-    modalQt = 1
+    modalQt = 1;
 
-    let key = event.target.closest('.pizza-item').getAttribute('data-key')
+    let key = event.target.closest(".pizza-item").getAttribute("data-key");
 
-    qS('.pizzaInfo h1').innerHTML = `${pizzaJson[key].name}`
-    qS('.pizzaInfo--desc').innerHTML = `${pizzaJson[key].description}`
-    qS('.pizzaBig img').src = `${pizzaJson[key].img}`
-    qS('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`
-    qS('.pizzaInfo--size.selected').classList.remove('selected')
-    qSA('.pizzaInfo--size').forEach((size, sizeIndex)=>{
-      if(sizeIndex == 2){
-        size.classList.add('selected')
+    modalKey = key;
+
+    qS(".pizzaInfo h1").innerHTML = `${pizzaJson[key].name}`;
+    qS(".pizzaInfo--desc").innerHTML = `${pizzaJson[key].description}`;
+    qS(".pizzaBig img").src = `${pizzaJson[key].img}`;
+    qS(".pizzaInfo--actualPrice").innerHTML = `R$ ${pizzaJson[
+      key
+    ].price.toFixed(2)}`;
+    qS(".pizzaInfo--size.selected").classList.remove("selected");
+    qSA(".pizzaInfo--size").forEach((size, sizeIndex) => {
+      if (sizeIndex == 2) {
+        size.classList.add("selected");
       }
-      size.querySelector('span').innerHTML = `${pizzaJson[key].sizes[sizeIndex]}`
-    })
+      size.querySelector(
+        "span"
+      ).innerHTML = `${pizzaJson[key].sizes[sizeIndex]}`;
+    });
 
-    qS('.pizzaInfo--qt').innerHTML = modalQt
+    qS(".pizzaInfo--qt").innerHTML = modalQt;
 
     qS(".pizzaWindowArea").style.display = "flex";
     setTimeout(() => {
@@ -45,25 +55,57 @@ pizzaJson.map((item, index) => {
   qS(".pizza-area").append(pizzaItem);
 });
 
-function closeOrderTab(){
+function closeOrderTab() {
   qS(".pizzaWindowArea").style.opacity = "0";
   setTimeout(() => {
-    qS(".piizaWindowArea").style.display = 'none';
-  }, 500)
+    qS(".pizzaWindowArea").style.display = "none";
+  }, 500);
 }
 
-qSA('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
-  item.addEventListener('click', closeOrderTab)
-})
-
-qS('.pizzaInfo--qtmenos').addEventListener('click', () => {
-  if(modalQt > 1){
-    modalQt--
-    qS('.pizzaInfo--qt').innerHTML = modalQt
+qSA(".pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton").forEach(
+  (item) => {
+    item.addEventListener("click", closeOrderTab);
   }
-})
+);
 
-qS('.pizzaInfo--qtmais').addEventListener('click', () => {
-  modalQt++
-  qS('.pizzaInfo--qt').innerHTML = modalQt
-})
+qS(".pizzaInfo--qtmenos").addEventListener("click", () => {
+  if (modalQt > 1) {
+    modalQt--;
+    qS(".pizzaInfo--qt").innerHTML = modalQt;
+  }
+});
+
+qS(".pizzaInfo--qtmais").addEventListener("click", () => {
+  modalQt++;
+  qS(".pizzaInfo--qt").innerHTML = modalQt;
+});
+
+qSA(".pizzaInfo--size").forEach((size, sizeIndex) => {
+  size.addEventListener("click", (e) => {
+    qS(".pizzaInfo--size.selected").classList.remove("selected");
+    size.classList.add("selected");
+  });
+});
+
+qS(".pizzaInfo--addButton").addEventListener("click", () => {
+  let selectedSize = parseInt(qS(".selected").getAttribute("data-key"));
+
+  let identifier = pizzaJson[modalKey].id + "@" + selectedSize;
+
+  let cartFinder = cart.findIndex((item) => {
+    return (item.identifier == identifier);
+  });
+
+  if (cartFinder > -1) {
+    cart[cartFinder].qt += modalQt
+  } else {
+    cart.push({
+      identifier: identifier,
+      id: pizzaJson[modalKey].id,
+      size: selectedSize,
+      qt: modalQt,
+    });
+  }
+
+  closeOrderTab();
+});
