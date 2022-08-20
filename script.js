1d453aad6a92424f81f5688a2f9bb6b1553388ae
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2022-08-19 22:58:43 -0300

    Pizza modal now show basic info.

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 const qS = (e) => document.querySelector(e);
 const qSA = (e) => document.querySelectorAll(e);
 
-pizzaJson.map((item, idex) => {
-    let pizzaItem = qS('.models .pizza-item').cloneNode(true)
+pizzaJson.map((item, index) => {
+  let pizzaItem = qS(".models .pizza-item").cloneNode(true);
 
-    pizzaItem.querySelector('.pizza-item--img img').src = item.img
+  pizzaItem.setAttribute('data-key', index);
 
-    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
-    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}` 
+  pizzaItem.querySelector(".pizza-item--img img").src = item.img;
 
-    pizzaItem.querySelector('a').addEventListener('click', (event) => {
-        event.preventDefault()
-        
-        qS('.pizzaWindowArea').style.display = 'flex'
-        setTimeout( () => {qS('.pizzaWindowArea').style.opacity = '1'}, 100)
-    })
+  pizzaItem.querySelector(".pizza-item--name").innerHTML = item.name;
+  pizzaItem.querySelector(".pizza-item--desc").innerHTML = item.description;
+  pizzaItem.querySelector(
+    ".pizza-item--price"
+  ).innerHTML = `R$ ${item.price.toFixed(2)}`;
 
-    qS('.pizza-area').append(pizzaItem)
-})
+  pizzaItem.querySelector("a").addEventListener("click", (event) => {
+    event.preventDefault();
+
+    let key = event.target.closest('.pizza-item').getAttribute('data-key')
+
+    qS('.pizzaInfo h1').innerHTML = `${pizzaJson[key].name}`
+    qS('.pizzaInfo--desc').innerHTML = `${pizzaJson[key].description}`
+    qS('.pizzaBig img').src = `${pizzaJson[key].img}`
+
+    qS(".pizzaWindowArea").style.display = "flex";
+    setTimeout(() => {
+      qS(".pizzaWindowArea").style.opacity = "1";
+    }, 100);
+  });
+
+  qS(".pizza-area").append(pizzaItem);
+});
+
